ee6816420fa93fe41d83616daf823b09bd117e7c
Jorge Aspiolea <aspiolea28@topfollowers.com>
2026-02-02 15:44:21 +0100

    fix: remove unused imports

--- tests/IntegrationTests/Api.Tests/Controllers/AuthControllerTests.cs
+++ tests/IntegrationTests/Api.Tests/Controllers/AuthControllerTests.cs
@@ -1,7 +1,5 @@
-using System.IdentityModel.Tokens.Jwt;
 using System.Net;
 using System.Net.Http.Json;
-using System.Security.Claims;
 using CoworkingManagement.Application.Common.Models;
 using CoworkingManagement.Domain.Entities;
 using CoworkingManagement.Infrastructure.Auth;
